edb1464d8a2e13001a93c8e39e06117c294820c3
Hideki Ikemoto <ikemo333@gmail.com>
2018-12-23 21:54:03 +0900

    add test.

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+    files: [
+      'apps/content_script/google.js',
+      'test/*.js'
+    ],
+    preprocessors: {
+      'apps/content_script/google.js': ['coverage']
+    },
+    reporters: ['mocha', 'coverage'],
+    browsers: ['ChromeHeadless'],
+    logLevel: config.LOG_INFO
+  })
+}
---
+++ test/ymd_to_mdy_test.js
@@ -0,0 +1,32 @@
+"use strict";
+describe("ymdToMdy", () => {
+    it("yyyy-mm-dd", () => {
+        expect(ymdToMdy("2016-01-02")).toBe("01/02/2016");
+        expect(ymdToMdy("2016/01/02")).toBe("01/02/2016");
+    });
+    it("yyyy-m-dd", () => {
+        expect(ymdToMdy("2016-1-02")).toBe("1/02/2016");
+        expect(ymdToMdy("2016/1/02")).toBe("1/02/2016");
+    });
+    it("yyyy-mm-d", () => {
+        expect(ymdToMdy("2016-01-2")).toBe("01/2/2016");
+        expect(ymdToMdy("2016/01/2")).toBe("01/2/2016");
+    });
+    it("yyyy-m-d", () => {
+        expect(ymdToMdy("2016-1-2")).toBe("1/2/2016");
+        expect(ymdToMdy("2016/1/2")).toBe("1/2/2016");
+    });
+    it("mm/dd/yyyy", () => {
+        expect(ymdToMdy("01/02/2016")).toBe("01/02/2016");
+    });
+    it("m/dd/yyyy", () => {
+        expect(ymdToMdy("1/02/2016")).toBe("1/02/2016");
+    });
+    it("mm/d/yyyy", () => {
+        expect(ymdToMdy("01/2/2016")).toBe("01/2/2016");
+    });
+    it("m/d/yyyy", () => {
+        expect(ymdToMdy("1/2/2016")).toBe("1/2/2016");
+    });
+});
+//# sourceMappingURL=ymd_to_mdy_test.js.map
