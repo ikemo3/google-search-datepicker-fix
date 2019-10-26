8b897582487427838576253999dea056bcc64268
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-26 11:17:34 +0900

    test: separate chrome.ts from test.ts

--- test/selenium/test.ts
+++ test/selenium/test.ts
@@ -1,26 +1,10 @@
-import { Builder, By, Capabilities } from 'selenium-webdriver';
-import { Options } from 'selenium-webdriver/chrome';
-import { readFileSync } from 'fs';
+import { By } from 'selenium-webdriver';
 import { strictEqual } from 'assert';
+import chromeDriver from './chrome';
 
 (async (): Promise<void> => {
     try {
-        const extension = readFileSync('tmp/workspace/google-search-datepicker.crx', 'base64');
-        const options = new Options()
-            .addExtensions(extension)
-            .windowSize({ width: 1280, height: 800 });
-
-        const capabilities = Capabilities.chrome();
-        capabilities.set('chromeOptions', {
-            args: [
-                '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-            ],
-        });
-
-        const driver = await new Builder()
-            .withCapabilities(capabilities)
-            .setChromeOptions(options)
-            .build();
+        const driver = chromeDriver();
 
         await driver.get('https://www.google.com/search?q=test');
 
