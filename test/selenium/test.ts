8a9e665e7177feb6a4f78c7455b90c0cbed28e1c
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-26 10:18:05 +0900

    test: exit 1 when test failed

--- test/selenium/test.ts
+++ test/selenium/test.ts
@@ -4,50 +4,56 @@ import { readFileSync } from 'fs';
 import { strictEqual } from 'assert';
 
 (async (): Promise<void> => {
-    const extension = readFileSync('tmp/workspace/google-search-datepicker.crx', 'base64');
-    const options = new Options()
-        .addExtensions(extension)
-        .windowSize({ width: 1280, height: 800 });
+    try {
+        const extension = readFileSync('tmp/workspace/google-search-datepicker.crx', 'base64');
+        const options = new Options()
+            .addExtensions(extension)
+            .windowSize({ width: 1280, height: 800 });
 
-    const capabilities = Capabilities.chrome();
-    capabilities.set('chromeOptions', {
-        args: [
-            '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-        ],
-    });
+        const capabilities = Capabilities.chrome();
+        capabilities.set('chromeOptions', {
+            args: [
+                '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+            ],
+        });
 
-    const driver = await new Builder()
-        .withCapabilities(capabilities)
-        .setChromeOptions(options)
-        .build();
+        const driver = await new Builder()
+            .withCapabilities(capabilities)
+            .setChromeOptions(options)
+            .build();
 
-    await driver.get('https://www.google.com/search?q=test');
+        await driver.get('https://www.google.com/search?q=test');
 
-    // click 'Tools'
-    const tool = driver.findElement(By.linkText('ツール'));
-    await tool.click();
+        // click 'Tools'
+        const tool = driver.findElement(By.linkText('ツール'));
+        await tool.click();
 
-    // click 'Any time'
-    const anyTime = driver.findElement(By.css("[aria-label='期間指定なし']"));
-    await driver.actions().pause(500).click(anyTime).perform();
+        // click 'Any time'
+        const anyTime = driver.findElement(By.css("[aria-label='期間指定なし']"));
+        await driver.actions().pause(500).click(anyTime).perform();
 
-    // click 'Custom range...'
-    const customRange = driver.findElement(By.id('cdrlnk'));
-    await driver.actions().pause(500).click(customRange).perform();
+        // click 'Custom range...'
+        const customRange = driver.findElement(By.id('cdrlnk'));
+        await driver.actions().pause(500).click(customRange).perform();
 
-    // set 'From'
-    const cdrMin = driver.findElement(By.id('cdr_min'));
-    await driver.actions().pause(500).click(cdrMin).sendKeys('2019/01/02')
-        .perform();
+        // set 'From'
+        const cdrMin = driver.findElement(By.id('cdr_min'));
+        await driver.actions().pause(500).click(cdrMin).sendKeys('2019/01/02')
+            .perform();
 
-    // click 'Go'
-    const goButton = driver.findElement(By.css("#cdr_frm input[value='選択']"));
-    await driver.actions().pause(500).click(goButton).perform();
+        // click 'Go'
+        const goButton = driver.findElement(By.css("#cdr_frm input[value='選択']"));
+        await driver.actions().pause(500).click(goButton).perform();
 
-    // assert time range label.
-    const timeRangeLabel = driver.findElement(By.className('hdtb-tsel'));
-    strictEqual(await timeRangeLabel.getAttribute('aria-label'), '2019年1月2日 – 今日');
+        // assert time range label.
+        const timeRangeLabel = driver.findElement(By.className('hdtb-tsel'));
+        strictEqual(await timeRangeLabel.getAttribute('aria-label'), '2019年1月2日 – 今日');
 
-    // quit driver.
-    await driver.close();
+        // quit driver.
+        await driver.close();
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+        process.exitCode = 1;
+    }
 })();
