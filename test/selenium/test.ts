276d96621922803b10db88089ecf7151c0d1804b
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-26 17:10:15 +0900

    style: reformat code

--- test/selenium/test.ts
+++ test/selenium/test.ts
@@ -3,35 +3,35 @@ import { strictEqual } from 'assert';
 import chromeDriver from './chrome';
 
 async function main(driver: WebDriver) {
-        await driver.get('https://www.google.com/search?q=test');
+    await driver.get('https://www.google.com/search?q=test');
 
-        // click 'Tools'
-        const tool = driver.findElement(By.linkText('ツール'));
-        await tool.click();
+    // click 'Tools'
+    const tool = driver.findElement(By.linkText('ツール'));
+    await tool.click();
 
-        // click 'Any time'
-        const anyTime = driver.findElement(By.css("[aria-label='期間指定なし']"));
-        await driver.actions().pause(500).click(anyTime).perform();
+    // click 'Any time'
+    const anyTime = driver.findElement(By.css("[aria-label='期間指定なし']"));
+    await driver.actions().pause(500).click(anyTime).perform();
 
-        // click 'Custom range...'
-        const customRange = driver.findElement(By.id('cdrlnk'));
-        await driver.actions().pause(500).click(customRange).perform();
+    // click 'Custom range...'
+    const customRange = driver.findElement(By.id('cdrlnk'));
+    await driver.actions().pause(500).click(customRange).perform();
 
-        // set 'From'
-        const cdrMin = driver.findElement(By.id('cdr_min'));
-        await driver.actions().pause(500).click(cdrMin).sendKeys('2019/01/02')
-            .perform();
+    // set 'From'
+    const cdrMin = driver.findElement(By.id('cdr_min'));
+    await driver.actions().pause(500).click(cdrMin).sendKeys('2019/01/02')
+        .perform();
 
-        // click 'Go'
-        const goButton = driver.findElement(By.css("#cdr_frm input[value='選択']"));
-        await driver.actions().pause(500).click(goButton).perform();
+    // click 'Go'
+    const goButton = driver.findElement(By.css("#cdr_frm input[value='選択']"));
+    await driver.actions().pause(500).click(goButton).perform();
 
-        // assert time range label.
-        const timeRangeLabel = driver.findElement(By.className('hdtb-tsel'));
-        strictEqual(await timeRangeLabel.getAttribute('aria-label'), '2019年1月2日 – 今日');
+    // assert time range label.
+    const timeRangeLabel = driver.findElement(By.className('hdtb-tsel'));
+    strictEqual(await timeRangeLabel.getAttribute('aria-label'), '2019年1月2日 – 今日');
 
-        // quit driver.
-        await driver.close();
+    // quit driver.
+    await driver.close();
 }
 
 (async (): Promise<void> => {
