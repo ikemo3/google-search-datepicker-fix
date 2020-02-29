678799139741dc7c6cdb1df0f7acf8f8be9e3979
Hideki Ikemoto <ikemo333@gmail.com>
2020-02-29 12:39:08 +0900

    build: add webpack.config.ts

---
+++ webpack.config.ts
@@ -0,0 +1,47 @@
+import { Configuration } from 'webpack';
+import { join } from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const config: Configuration = {
+    mode: 'development',
+    entry: {
+        google: join(__dirname, 'apps', 'content_script', 'google.ts'),
+    },
+    output: {
+        path: join(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            {
+                from: 'apps/manifest.json',
+                to: '',
+            },
+            {
+                from: 'apps/_locales',
+                to: '_locales',
+            },
+            {
+                from: 'apps/icons',
+                to: 'icons',
+            },
+            {
+                from: 'apps/.web-extension-id',
+                to: '',
+            },
+        ]),
+    ],
+    devtool: 'source-map',
+};
+
+export default config;
