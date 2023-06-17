cb66bd4086af08223aed23b29aa6fe5cb07033bd
Hideki Ikemoto <ikemo333@gmail.com>
2023-06-18 08:40:12 +0900

    style: Remove printWidth and tabWidth from .prettierrc.json

--- scripts/archive.ts
+++ scripts/archive.ts
@@ -1,74 +1,95 @@
-import { copyFileSync, cpSync, existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from "fs";
+import {
+  copyFileSync,
+  cpSync,
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  rmSync,
+  writeFileSync,
+} from "fs";
 import path from "path";
 import { execSync } from "child_process";
 import { zip } from "zip-a-folder";
 
 function clearDirectory(path: string) {
-    if (existsSync(path)) {
-        rmSync(path, { recursive: true });
-    }
+  if (existsSync(path)) {
+    rmSync(path, { recursive: true });
+  }
 }
 
 function getPackageName(projectDir: string) {
-    const packageJsonPath = projectDir + "/package.json";
-    const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
-    return packageJson.name;
+  const packageJsonPath = projectDir + "/package.json";
+  const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
+  return packageJson.name;
 }
 
 function getExtensionId(projectDir: string) {
-    const extensionIdPath = projectDir + "/apps/.web-extension-id";
-    const webExtensionId = readFileSync(extensionIdPath).toString();
-    return webExtensionId.split("\n")[2];
+  const extensionIdPath = projectDir + "/apps/.web-extension-id";
+  const webExtensionId = readFileSync(extensionIdPath).toString();
+  return webExtensionId.split("\n")[2];
 }
 
 async function createChromeExtension(packageName: string, projectDir: string) {
-    console.log(`Create ${packageName}.zip`);
-    await zip(projectDir + "/dist-chrome", projectDir + `/tmp/workspace/${packageName}-chrome.zip`);
+  console.log(`Create ${packageName}.zip`);
+  await zip(
+    projectDir + "/dist-chrome",
+    projectDir + `/tmp/workspace/${packageName}-chrome.zip`
+  );
 }
 
 async function createFirefoxExtension(packageName: string, projectDir: string) {
-    // load manifest.json
-    const extensionId = getExtensionId(projectDir);
-    console.log(`Extension ID: ${extensionId}`);
+  // load manifest.json
+  const extensionId = getExtensionId(projectDir);
+  console.log(`Extension ID: ${extensionId}`);
 
-    const manifestJsonPath = projectDir + "/apps/manifest.json";
-    const manifestJson = JSON.parse(readFileSync(manifestJsonPath).toString());
-    manifestJson.browser_specific_settings = { gecko: { id: extensionId } };
+  const manifestJsonPath = projectDir + "/apps/manifest.json";
+  const manifestJson = JSON.parse(readFileSync(manifestJsonPath).toString());
+  manifestJson.browser_specific_settings = { gecko: { id: extensionId } };
 
-    // write manifest.json
-    const manifestJsonWritePath = projectDir + "/dist-firefox/manifest.json";
-    writeFileSync(manifestJsonWritePath, JSON.stringify(manifestJson, null, 2));
+  // write manifest.json
+  const manifestJsonWritePath = projectDir + "/dist-firefox/manifest.json";
+  writeFileSync(manifestJsonWritePath, JSON.stringify(manifestJson, null, 2));
 
-    // create Firefox extension(xpi)
-    await zip(projectDir + "/dist-firefox", projectDir + `/tmp/workspace/${packageName}-firefox.xpi`);
+  // create Firefox extension(xpi)
+  await zip(
+    projectDir + "/dist-firefox",
+    projectDir + `/tmp/workspace/${packageName}-firefox.xpi`
+  );
 }
 
 async function main() {
-    // create if there is no dist directory
-    const projectDir = path.dirname(__dirname);
-    clearDirectory(projectDir + "/dist-chrome");
-    clearDirectory(projectDir + "/dist-firefox");
-    clearDirectory(projectDir + "/tmp");
-    mkdirSync(projectDir + "/tmp");
-    mkdirSync(projectDir + "/tmp/workspace");
+  // create if there is no dist directory
+  const projectDir = path.dirname(__dirname);
+  clearDirectory(projectDir + "/dist-chrome");
+  clearDirectory(projectDir + "/dist-firefox");
+  clearDirectory(projectDir + "/tmp");
+  mkdirSync(projectDir + "/tmp");
+  mkdirSync(projectDir + "/tmp/workspace");
 
-    // copy resources
-    execSync("webpack", { stdio: "inherit" });
-    cpSync(projectDir + "/dist", projectDir + "/dist-chrome", { recursive: true });
-    cpSync(projectDir + "/dist", projectDir + "/dist-firefox", { recursive: true });
-    copyFileSync(projectDir + "/apps/.web-extension-id", projectDir + "/dist-firefox/.web-extension-id");
+  // copy resources
+  execSync("webpack", { stdio: "inherit" });
+  cpSync(projectDir + "/dist", projectDir + "/dist-chrome", {
+    recursive: true,
+  });
+  cpSync(projectDir + "/dist", projectDir + "/dist-firefox", {
+    recursive: true,
+  });
+  copyFileSync(
+    projectDir + "/apps/.web-extension-id",
+    projectDir + "/dist-firefox/.web-extension-id"
+  );
 
-    // load package.json
-    const packageName = getPackageName(projectDir);
+  // load package.json
+  const packageName = getPackageName(projectDir);
 
-    // create Chrome extension
-    await createChromeExtension(packageName, projectDir);
+  // create Chrome extension
+  await createChromeExtension(packageName, projectDir);
 
-    // create Firefox extension
-    await createFirefoxExtension(packageName, projectDir);
+  // create Firefox extension
+  await createFirefoxExtension(packageName, projectDir);
 }
 
 main().catch((err) => {
-    console.error(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 });
--- test/github_release.test.ts
+++ test/github_release.test.ts
@@ -1,76 +1,90 @@
 import { configureGhrOption, isError } from "../scripts/libs";
 
 describe("configureGhrOption", () => {
-    test("master branch", () => {
-        expect(configureGhrOption("master", "", "1.0.0", "", "1.0.0")).toStrictEqual({
-            tag: "snapshot",
-            name: "v1.0.0-snapshot",
-            ghrOptions: ["-prerelease", "-recreate"],
-        });
+  test("master branch", () => {
+    expect(
+      configureGhrOption("master", "", "1.0.0", "", "1.0.0")
+    ).toStrictEqual({
+      tag: "snapshot",
+      name: "v1.0.0-snapshot",
+      ghrOptions: ["-prerelease", "-recreate"],
     });
+  });
 
-    test("prototype branch", () => {
-        expect(configureGhrOption("v1.0.0-prototype", "", "1.0.0", "", "1.0.0")).toStrictEqual({
-            tag: "v1.0.0-prototype",
-            name: "v1.0.0-prototype",
-            ghrOptions: ["-prerelease", "-recreate"],
-        });
+  test("prototype branch", () => {
+    expect(
+      configureGhrOption("v1.0.0-prototype", "", "1.0.0", "", "1.0.0")
+    ).toStrictEqual({
+      tag: "v1.0.0-prototype",
+      name: "v1.0.0-prototype",
+      ghrOptions: ["-prerelease", "-recreate"],
     });
+  });
 
-    test("tag", () => {
-        expect(configureGhrOption("", "v1.0.0", "1.0.0", "", "1.0.0")).toStrictEqual({
-            tag: "v1.0.0",
-            name: "v1.0.0",
-            ghrOptions: ["-recreate"],
-        });
+  test("tag", () => {
+    expect(
+      configureGhrOption("", "v1.0.0", "1.0.0", "", "1.0.0")
+    ).toStrictEqual({
+      tag: "v1.0.0",
+      name: "v1.0.0",
+      ghrOptions: ["-recreate"],
     });
+  });
 
-    test("ignore", () => {
-        expect(configureGhrOption("", "snapshot", "1.0.0", "", "1.0.0")).toStrictEqual({
-            message: "ignore `snapshot` tag (already released)",
-            exitCode: 0,
-        });
+  test("ignore", () => {
+    expect(
+      configureGhrOption("", "snapshot", "1.0.0", "", "1.0.0")
+    ).toStrictEqual({
+      message: "ignore `snapshot` tag (already released)",
+      exitCode: 0,
     });
+  });
 
-    test("tag != 'v' + manifest_version", () => {
-        expect(configureGhrOption("", "v1.0.1", "1.0.0", "", "1.0.0")).toStrictEqual({
-            message: "tag(v1.0.1) != 'v' + manifest_version(1.0.0)",
-            exitCode: 1,
-        });
+  test("tag != 'v' + manifest_version", () => {
+    expect(
+      configureGhrOption("", "v1.0.1", "1.0.0", "", "1.0.0")
+    ).toStrictEqual({
+      message: "tag(v1.0.1) != 'v' + manifest_version(1.0.0)",
+      exitCode: 1,
     });
+  });
 
-    test("manifest_version != package_version", () => {
-        expect(configureGhrOption("", "v1.0.1", "1.0.1", "", "1.0.0")).toStrictEqual({
-            message: "manifest_version(1.0.1) != package_version(1.0.0)",
-            exitCode: 1,
-        });
+  test("manifest_version != package_version", () => {
+    expect(
+      configureGhrOption("", "v1.0.1", "1.0.1", "", "1.0.0")
+    ).toStrictEqual({
+      message: "manifest_version(1.0.1) != package_version(1.0.0)",
+      exitCode: 1,
     });
+  });
 
-    test("manifest_version_name exists", () => {
-        expect(configureGhrOption("", "v1.0.0", "1.0.0", "snapshot", "1.0.0")).toStrictEqual({
-            message: "version_name exists",
-            exitCode: 1,
-        });
+  test("manifest_version_name exists", () => {
+    expect(
+      configureGhrOption("", "v1.0.0", "1.0.0", "snapshot", "1.0.0")
+    ).toStrictEqual({
+      message: "version_name exists",
+      exitCode: 1,
     });
+  });
 });
 
 describe("isError", () => {
-    test("isNormal", () => {
-        expect(
-            isError({
-                tag: "snapshot",
-                name: "v1.0.0-snapshot",
-                ghrOptions: ["-prerelease", "-recreate"],
-            })
-        ).toStrictEqual(false);
-    });
+  test("isNormal", () => {
+    expect(
+      isError({
+        tag: "snapshot",
+        name: "v1.0.0-snapshot",
+        ghrOptions: ["-prerelease", "-recreate"],
+      })
+    ).toStrictEqual(false);
+  });
 
-    test("isError", () => {
-        expect(
-            isError({
-                message: "version_name exists",
-                exitCode: 1,
-            })
-        ).toStrictEqual(true);
-    });
+  test("isError", () => {
+    expect(
+      isError({
+        message: "version_name exists",
+        exitCode: 1,
+      })
+    ).toStrictEqual(true);
+  });
 });
