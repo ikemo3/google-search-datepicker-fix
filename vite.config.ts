ad7fa2ab2fd2d6092ba340ebb6ce02725c0c9f7a
Hideki Ikemoto <ikemo333@gmail.com>
2023-07-20 22:48:54 +0900

    migrate webpack to vite

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+// https://ja.vitejs.dev/guide/backend-integration.html
+export default defineConfig({
+  build: {
+    target: "es2022",
+    minify: false,
+    cssMinify: false,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        google: resolve(__dirname, "apps/content_script/google.ts"),
+      },
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+  plugins: [],
+});
